4f28fa2f0bc09696aa4172ff5c97d8580ed8827b
Zeztron <pateltharsh@gmail.com>
2019-06-16 18:35:32 -0400

    created description and animation

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { 
+  Text, 
+  TouchableWithoutFeedback, 
+  View,
+  LayoutAnimation
+} from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from "../actions";
 
 class ListItem extends Component {
+
+    componentDidUpdate() {
+		LayoutAnimation.spring();
+	}
+  
+    renderDescription() {
+		const { descriptionStyle } = styles;
+        const { library, expanded } = this.props;
+
+        if (expanded) {
+            return (
+              <CardSection>
+                <Text style={descriptionStyle}>{library.description}</Text>
+              </CardSection>
+            );
+        }
+    }
+
     render() {
         const { titleStyle } = styles;
+        const { id, title } = this.props.library;
 
         return (
-            <CardSection>
-                <Text style={titleStyle}>{this.props.library.title}</Text>
-            </CardSection>
-        )
+          <TouchableWithoutFeedback
+            onPress={() => this.props.selectedLibrary(id)}
+          >
+            <View>
+              <CardSection>
+                <Text style={titleStyle}>
+                  {title}
+                </Text>
+              </CardSection>
+              {this.renderDescription()}
+            </View>
+          </TouchableWithoutFeedback>
+        );
     }
 }
 
@@ -20,8 +53,18 @@ const styles = {
     titleStyle: {
         fontSize: 18,
         paddingLeft: 15
-    }
+	},
+	descriptionStyle: {
+		paddingLeft: 10,
+		paddingRight: 10
+	}
 }
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = (state, ownProps) => {
+    const expanded = state.selectedLibraryId === ownProps.library.id;
+
+    return { expanded };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
 
